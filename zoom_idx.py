e5a31618e79f2ef4a87107f711f86709246da370
kjh <kjh990705@naver.com>
2024-11-20 07:37:23 +0000

    4:37

--- zoom_idx.py
+++ zoom_idx.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 def plot_page_access_counts(input_file):
-    output_image = "zoom_pg_idx_acc_cnt.png"
+    output_image = "zoom_pg_idx_100.png"
     page_indices = []
     access_counts = []
 
@@ -14,7 +14,7 @@ def plot_page_access_counts(input_file):
                 page_indices.append(page)
                 access_counts.append(count)
                 i += 1
-                if (i > 1000):
+                if (i > 100):
                     break
     except FileNotFoundError:
         print(f"Error: File not found at {input_file}. Please ensure the file exists.")
@@ -22,7 +22,7 @@ def plot_page_access_counts(input_file):
 
     # 그래프 그리기
     plt.figure(figsize=(12, 6))
-    plt.plot(page_indices, access_counts, marker='o', linestyle='-', linewidth=1)
+    plt.plot(page_indices, access_counts, marker='o',markersize=1, linestyle='-', linewidth=1)
     plt.title("Page Index vs Access Count")
     plt.xlabel("Page Index")
     plt.ylabel("Access Count")
